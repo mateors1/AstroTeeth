73a1b40626b88136add7c59606e56001dea91507
Mateo Rendon <mateorendon1@gmail.com>
2024-01-23 21:01:20 -0500

    fixes post blackout

--- Assets/Scripts/PasserbySystems/PasserbySystemTest.cs
+++ Assets/Scripts/PasserbySystems/PasserbySystemTest.cs
@@ -7,11 +7,6 @@ public class PasserbySystemTest : MonoBehaviour
 
     [SerializeField]
     string targetTag;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other)
